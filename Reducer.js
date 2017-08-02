c62d33c587ab9014b6f5692d91039f9e5f95f0c0
LizPeng <lizhipeng2013@163.com>
2017-08-02 14:34:43 +0800

    add func

---
+++ Reducer.js
@@ -0,0 +1,33 @@
+const chatReducer = (state = defaultState, action = {}) => {
+  return {
+    chatLog: chatLog(state.chatLog, action),
+    statusMessage: statusMessage(state.statusMessage, action);
+    userName: userName(state.userName, action)
+  }
+};
+
+import { combineReducers } from 'redux';
+
+const chatReducer1 = combineReducers({
+  chatLog,
+  statusMessage,
+  userName
+})
+export default todoApp
+/*
+上面的代码通过combineReducers方法将三个子Reducer合并成一个大函数。
+这种写法有一个前提，就是State的属性名必须与子Reducer同名
+*/
+//不同名要采用下面的写法
+const reducer2 = combineReducers({
+  a:doSomethingA,
+  b:processB,
+  c:c
+})
+
+//combinerReducer的简单实现
+const combineReducers = reducers => {
+  return (state ={}, action) => {
+    
+  }
+}
